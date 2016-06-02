e62611d5a07554191e5a969e5f7386c7ed39cb77
liuergua <841103312@qq.com>
2016-06-02 11:29:57 +0800

    添加BaseActivity

---
+++ app/src/main/java/com/liukun/teabaike/com/liukun/teabaike/activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.liukun.teabaike.com.liukun.teabaike.activity;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.liukun.teabaike.R;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
