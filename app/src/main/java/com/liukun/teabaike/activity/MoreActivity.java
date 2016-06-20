5dfb69b924fb9a10e4e4764febc1710e71bd92ec
liuergua <841103312@qq.com>
2016-06-20 23:16:44 +0800

    添加部分“更多”界面代码

--- app/src/main/java/com/liukun/teabaike/activity/MoreActivity.java
+++ app/src/main/java/com/liukun/teabaike/activity/MoreActivity.java
@@ -1,10 +1,16 @@
 package com.liukun.teabaike.activity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.liukun.teabaike.R;
 
-public class MoreActivity extends  BaseActivity{
+public class MoreActivity extends  BaseActivity implements View.OnClickListener,View.OnFocusChangeListener{
+    private LinearLayout searchLayout;
+    private Button goSearch;
+    private EditText keyWord;
     @Override
     protected int getLayoutId() {
         return R.layout.more_layout;
@@ -12,7 +18,12 @@ public class MoreActivity extends  BaseActivity{
 
     @Override
     protected void initView() {
+        searchLayout = (LinearLayout) findViewById(R.id.searchLayout);
+        goSearch= (Button) findViewById(R.id.goSearch);
+        keyWord= (EditText) findViewById(R.id.keyWorld);
 
+        goSearch.setOnClickListener(this);
+        keyWord.setOnFocusChangeListener(this);
     }
 
     @Override
@@ -24,5 +35,23 @@ public class MoreActivity extends  BaseActivity{
     }
     public void more_home(View view){
         finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.goSearch:
+                showShortToast("search");
+                break;
+        }
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus){
+            searchLayout.setBackgroundResource(R.drawable.linear_pressed);
+        }else {
+            searchLayout.setBackgroundResource(R.drawable.linear_on);
+        }
     }
 }
